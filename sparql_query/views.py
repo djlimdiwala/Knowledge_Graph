eaf6b16e1fbff7ffbfc89ba03fec37555394c75a
djlimdiwala <dhaval.limdiwala@gmail.com>
2019-05-31 17:01:47 +0530

    version1

--- sparql_query/views.py
+++ sparql_query/views.py
@@ -1,4 +1,4 @@
-from . queryyy import make_query
+from . query_maker import make_query
 from django.utils.safestring import mark_safe
 from django.shortcuts import render
 import json
@@ -8,11 +8,21 @@ def index(request):
 	stringg = "Your query will be generated here...."
 	if(request.GET.get('my_btn')):
 		output_variable = "accident"
-		city = request.GET.get('City')
-		country = ""
-		RunwaySurface = request.GET.get('Runway_SUrface')
-		Aircraftmodel = request.GET.get('Aircraft_model')
+
+		city = request.GET.get('City').replace(" ", "")
+		RunwaySurface = request.GET.get('Runway_Surface').replace(" ", "")
+		pilot_certi = str(request.GET.get('pilot_certificate')).replace(" ", "")
+		Aircraft_manufacturer = str(request.GET.get('aircraft_manufacturer')).replace(" ", "")
+		Engine_manufacturer = str(request.GET.get('engine_manufacturer')).replace(" ", "")
+
+		pilot_age_min =  str(request.GET.get('Pilot_age_min')).replace(" ", "")
+		pilot_age_max =  str(request.GET.get('Pilot_age_max')).replace(" ", "")
 		agee = "30"
-		stringg = make_query(output_variable, city, country, RunwaySurface, Aircraftmodel, agee)
+		Aircraftmodel = ""
+		country = ""
+
+		# print (Engine_manufacturer)
+		# print ("here")
+		stringg = make_query(output_variable, str(city), country, str(RunwaySurface), Aircraftmodel, pilot_age_min, pilot_age_max, pilot_certi, Aircraft_manufacturer, Engine_manufacturer)
 
 	return render(request, "sparql_query/index.html", {'stringg_abc' : mark_safe(stringg)})
